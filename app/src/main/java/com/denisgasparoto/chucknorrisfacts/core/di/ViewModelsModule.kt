2e27b1f65d27b53e00aa4cb0494216bb5081e517
Denis Gasparoto <denis.rosa@uds.com.br>
2020-04-06 17:56:48 -0300

    feat: implementing MVVM Architecture

---
+++ app/src/main/java/com/denisgasparoto/chucknorrisfacts/core/di/ViewModelsModule.kt
@@ -0,0 +1,10 @@
+package com.denisgasparoto.chucknorrisfacts.core.di
+
+import org.koin.dsl.module
+
+/**
+ * @author Denis Gasparoto on 27/03/2020.
+ */
+internal val viewModelsModule = module {
+
+}
