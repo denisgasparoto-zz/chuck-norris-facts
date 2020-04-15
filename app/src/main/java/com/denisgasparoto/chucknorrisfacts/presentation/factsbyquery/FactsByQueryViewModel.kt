b40dc627870653a9f5406a6e891c1934c88e23f2
Denis Gasparoto <denis.rosa@uds.com.br>
2020-04-14 21:11:52 -0300

    feat: update code coverage

--- app/src/main/java/com/denisgasparoto/chucknorrisfacts/presentation/factsbyquery/FactsByQueryViewModel.kt
+++ app/src/main/java/com/denisgasparoto/chucknorrisfacts/presentation/factsbyquery/FactsByQueryViewModel.kt
@@ -51,8 +51,9 @@ class FactsByQueryViewModel(
                                     updatedAt = it.updatedAt,
                                     url = it.url,
                                     fact = it.fact,
-                                    factLength = if (it.fact.length >= FACT_LENGTH_TEXT_SIZE_MANAGER) R.dimen.text_normal
-                                    else R.dimen.text_huge
+                                    factLength = if (it.fact.length >= FACT_LENGTH_TEXT_SIZE_MANAGER) {
+                                        R.dimen.text_normal
+                                    } else R.dimen.text_huge
                                 )
                             }
                         )
